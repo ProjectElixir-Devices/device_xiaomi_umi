8766f9dcea90116342ac27504e4fc700fa7e27da
adrian-8901 <adrianlam230@gmail.com>
2021-11-13 19:05:51 +0800

    umi: Bringup CrystalOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The crystalOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_umi.mk
+    $(LOCAL_DIR)/crystal_umi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_umi-eng \
-    lineage_umi-user \
-    lineage_umi-userdebug
+    crystal_umi-eng \
+    crystal_umi-user \
+    crystal_umi-userdebug

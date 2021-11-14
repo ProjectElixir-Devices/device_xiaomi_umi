b2a1ab15105a3d9a6810db8a51e4ec3454c7284c
adrian-8901 <adrianlam230@gmail.com>
2021-11-14 18:11:59 +0800

    [DNM]umi: Build with broken ELF

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
+
+
 DEVICE_PATH := device/xiaomi/umi
 
 # AVB
